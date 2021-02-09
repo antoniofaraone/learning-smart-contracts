536741d535c94d1924654f70bb98189549aa46b6
Antonio Faraone <antoniofaraone93@gmail.com>
2021-02-09 10:13:48 +0100

    Prove

--- fundraiser/client/src/NewFundraiser.js
+++ fundraiser/client/src/NewFundraiser.js
@@ -1,18 +1,17 @@
-
 import React, { useState, useEffect } from "react";
-import FormControl from '@material-ui/core/FormControl';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import FormControl from "@material-ui/core/FormControl";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
 import getWeb3 from "./utils/getWeb3";
 import FactoryContract from "./contracts/FundraiserFactory.json";
-import Web3 from 'web3'
+import Web3 from "web3";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -24,64 +23,67 @@ const useStyles = makeStyles(theme => ({
   menu: {
     width: 200,
   },
+  button: { margin: theme.spacing(1) },
+  input: { display: "none" },
 }));
 
 const NewFundraiser = () => {
-const [labelWidth, setLabelWidth] = React.useState(0);
-const labelRef = React.useRef(null);
-const classes = useStyles();
-const [ web3, setWeb3 ] = useState(null)
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  const labelRef = React.useRef(null);
+  const classes = useStyles();
+  const [web3, setWeb3] = useState(null);
 
-useEffect(() => {
-  const init = async() => {
-    try {
-      const web3 = await getWeb3();
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = FactoryContract.networks[networkId];
-      const accounts = await web3.eth.getAccounts();
-      const instance = new web3.eth.Contract(
-        FactoryContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3 = await getWeb3();
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = FactoryContract.networks[networkId];
+        const accounts = await web3.eth.getAccounts();
+        const instance = new web3.eth.Contract(
+          FactoryContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
 
-      setWeb3(web3)
-      setContract(instance)
-      setAccounts(accounts)
+        setWeb3(web3);
+        setContract(instance);
+        setAccounts(accounts);
+      } catch (error) {
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.error(error);
+      }
+    };
+    init();
+  }, []);
 
-    } catch(error) {
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  }
-  init();
-}, []);
+  const [name, setFundraiserName] = useState(null);
+  const [website, setFundraiserWebsite] = useState(null);
+  const [description, setFundraiserDescription] = useState(null);
+  const [image, setImage] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [custodian, setCustodian] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [funds, setFunds] = useState(null);
 
-const [ name, setFundraiserName ] = useState(null)
-const [ website, setFundraiserWebsite ] = useState(null)
-const [ description, setFundraiserDescription ] = useState(null)
-const [ image, setImage ] = useState(null)
-const [ address, setAddress ] = useState(null)
-const [ contract, setContract] = useState(null)
-const [ accounts, setAccounts ] = useState(null)
+  const handleSubmit = async () => {
+    const imageURL = image;
+    const url = website;
+    const beneficiary = address;
+    console.log("uso web3", web3);
+    const currentUser = await web3.currentProvider.selectedAddress;
+    const funds = await contract.methods.fundraisers(10, 0).call();
+    setFunds(funds);
+    console.log("funds", funds);
 
-const handleSubmit = async () => {
-  const imageURL = image
-  const url = website
-  const beneficiary = address
-  const currentUser = await web3.currentProvider.selectedAddress
+    const transaction = await contract.methods
+      .createFundraiser(name, url, imageURL, description, beneficiary)
+      .send({ from: accounts[0] });
 
-  const transaction = await contract.methods.createFundraiser(
-    name,
-    url,
-    imageURL,
-    description,
-    beneficiary
-  ).send({ from: accounts[0] })
-
-  alert('Successfully created fundraiser')
-}
+    alert("Successfully created fundraiser");
+  };
 
   return (
     <div className="create-fundraiser-container">
@@ -95,7 +97,7 @@ const handleSubmit = async () => {
         margin="normal"
         onChange={(e) => setFundraiserName(e.target.value)}
         variant="outlined"
-        inputProps={{ 'aria-label': 'bare' }}
+        inputProps={{ "aria-label": "bare" }}
       />
 
       <label>Website</label>
@@ -106,7 +108,7 @@ const handleSubmit = async () => {
         margin="normal"
         onChange={(e) => setFundraiserWebsite(e.target.value)}
         variant="outlined"
-        inputProps={{ 'aria-label': 'bare' }}
+        inputProps={{ "aria-label": "bare" }}
       />
 
       <label>Description</label>
@@ -117,7 +119,7 @@ const handleSubmit = async () => {
         margin="normal"
         onChange={(e) => setFundraiserDescription(e.target.value)}
         variant="outlined"
-        inputProps={{ 'aria-label': 'bare' }}
+        inputProps={{ "aria-label": "bare" }}
       />
 
       <label>Image</label>
@@ -128,7 +130,7 @@ const handleSubmit = async () => {
         margin="normal"
         onChange={(e) => setImage(e.target.value)}
         variant="outlined"
-        inputProps={{ 'aria-label': 'bare' }}
+        inputProps={{ "aria-label": "bare" }}
       />
 
       <label>Address</label>
@@ -139,7 +141,18 @@ const handleSubmit = async () => {
         margin="normal"
         onChange={(e) => setAddress(e.target.value)}
         variant="outlined"
-        inputProps={{ 'aria-label': 'bare' }}
+        inputProps={{ "aria-label": "bare" }}
+      />
+
+      <label>Custodian</label>
+      <TextField
+        id="outlined-bare"
+        className={classes.textField}
+        placeholder="Fundraiser Custodian"
+        margin="normal"
+        onChange={(e) => setCustodian(e.target.value)}
+        variant="outlined"
+        inputProps={{ "aria-label": "bare" }}
       />
 
       <Button
@@ -149,8 +162,7 @@ const handleSubmit = async () => {
         Submit
       </Button>
     </div>
-  )
-}
-
+  );
+};
 
 export default NewFundraiser;
